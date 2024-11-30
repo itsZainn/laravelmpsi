dd6cf5c7f66a7194af14961a6ecbc957a5a1d584
itsZainn <afanashabi@gmail.com>
2024-11-30 14:09:33 +0700

    menambah span
    menambah span pada judul

--- resources/views/catalogue.blade.php
+++ resources/views/catalogue.blade.php
@@ -159,7 +159,11 @@
 
         <footer class="bg-gray-200">
             <div class="container mx-auto px-6 py-3 flex justify-between items-center">
-                <a href="#" class="text-xl font-bold text-gray-500 hover:text-gray-400">Cahya Aquatic</a>
+                <a href="#" class="text-xl font-bold text-gray-500 hover:text-gray-400">
+                    <span class="text-blue-500">
+                        Cahya Aquatic
+                    </span>
+                </a>
                 <p class="py-2 text-gray-500 sm:py-0">Sumberagung, Wates, Kediri</p>
             </div>
         </footer>
